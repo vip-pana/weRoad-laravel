bd97a22c129e2e9864a018daa1c3ce25312b8430
panacciullivincenzo <panacciullivincenzo@gmail.com>
2024-04-03 07:12:59 +0200

    fix Factories: add userRole property on UserFactory

--- weRoad/database/factories/UserFactory.php
+++ weRoad/database/factories/UserFactory.php
@@ -2,6 +2,7 @@
 
 namespace Database\Factories;
 
+use App\Enums\UserRole;
 use Illuminate\Database\Eloquent\Factories\Factory;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Str;
@@ -26,6 +27,7 @@ class UserFactory extends Factory
         return [
             'name' => fake()->name(),
             'email' => fake()->unique()->safeEmail(),
+            'role' =>  $this->faker->randomElement(UserRole::class),
             'email_verified_at' => now(),
             'password' => static::$password ??= Hash::make('password'),
             'remember_token' => Str::random(10),
