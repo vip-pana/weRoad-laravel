d7ad2bcfd1cd2833db19f5b22b5f71d65e0e873e
panacciullivincenzo <panacciullivincenzo@gmail.com>
2024-04-04 23:57:41 +0200

    fix tests problem with roles

--- weRoad/tests/Feature/AuthorizationTest.php
+++ weRoad/tests/Feature/AuthorizationTest.php
@@ -16,20 +16,20 @@ test('unauthenticated user can not access travels.create')
     ->get('/travels/create')->assertStatus(302)->assertRedirect('login');
 
 test('unauthenticated user can not access tours.create', function () {
-    $this->get('/travels/'.$this->travel->slug.'/tours/create')->assertStatus(302)->assertRedirect('login');
+    $this->get('/travels/' . $this->travel->slug . '/tours/create')->assertStatus(302)->assertRedirect('login');
 });
 
 test('unauthenticated user can not access tours.edit', function () {
     $tour = $this->travel->tours[0];
-    $this->get('/tours/'.$tour.'/edit')->assertStatus(302)->assertRedirect('login');
+    $this->get('/tours/' . $tour . '/edit')->assertStatus(302)->assertRedirect('login');
 });
 
 test('unauthenticated user can not access travels.edit', function () {
-    $this->get('/tours/'.$this->travel->slug.'/edit')->assertStatus(302)->assertRedirect('login');
+    $this->get('/tours/' . $this->travel->slug . '/edit')->assertStatus(302)->assertRedirect('login');
 });
 
 test('admin user can not access travels.edit', function () {
-    $this->actingAs($this->adminUser)->get('/travels/'.$this->travel->slug.'/tours/create')
+    $this->actingAs($this->adminUser)->get('/travels/' . $this->travel->slug . '/edit')
         ->assertStatus(403);
 });
 
@@ -38,4 +38,8 @@ test('login access redirect to dashboard', function () {
         'email' => $this->adminUser->email,
         'password' => 'password',
     ])->assertStatus(302)->assertRedirect('/');
+});
+
+test('admin user can access travels.create', function () {
+    $this->actingAs($this->adminUser)->get('/travels/create')->assertStatus(200);
 });
