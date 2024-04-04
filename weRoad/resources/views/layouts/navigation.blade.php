2c3c9c9a32ec9a5442183246c4f210cf147822d6
panacciullivincenzo <panacciullivincenzo@gmail.com>
2024-04-04 13:21:31 +0200

    navbar: deleted visible link of profile on navbar

--- weRoad/resources/views/layouts/navigation.blade.php
+++ weRoad/resources/views/layouts/navigation.blade.php
@@ -46,10 +46,6 @@
                         </x-slot>
 
                         <x-slot name="content">
-                            <x-dropdown-link :href="route('profile.edit')">
-                                {{ __('Profile') }}
-                            </x-dropdown-link>
-
                             <!-- Authentication -->
                             <form method="POST" action="{{ route('logout') }}" id="logout-form">
                                 @csrf
