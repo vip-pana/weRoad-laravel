5652f7176dc0ae8388ab8c1476fbb284d2d001f9
panacciullivincenzo <panacciullivincenzo@gmail.com>
2024-04-08 10:53:12 +0200

    Revert "fix frontend: fix rows dimensions and don't show details if there aren't"
    This reverts commit 356bdb816605e2cdf7b0a189acbb3791e9f8355c.

--- resources/views/components/travel-card.blade.php
+++ resources/views/components/travel-card.blade.php
@@ -2,10 +2,10 @@
 
 <x-card>
     <div class="flex justify-between">
-        <h3 class="text-2xl w-96">
+        <h3 class="text-2xl ">
             {{ $travel->name }}
         </h3>
-        <div>
+        <div class="flex flex-col">
             @php
                 $startingDates = $travel->tours->pluck('startingDate');
                 $oldestStartingDate = $startingDates->min();
@@ -19,16 +19,12 @@
                 $lowestPriceTour = intval($lowestPriceTour / 100);
                 $formattedLowestPrice = number_format($lowestPriceTour, -4, ',', '.');
             @endphp
-            @if (count($travel->tours) != 0)
-                <p>Next Tour: {{ \Carbon\Carbon::parse($oldestStartingDate)->format('m/d/Y') }}</p>
-                <p>Last Tour: {{ \Carbon\Carbon::parse($newestStartingDate)->format('m/d/Y') }}</p>
-                <p>Starting from: {{ $formattedLowestPrice }}€</p>
-                <p>Max price: {{ $formattedMaxPrice }}€</p>
-            @else
-                No tours available
-            @endif
+            <p>Next Tour: {{ \Carbon\Carbon::parse($oldestStartingDate)->format('m/d/Y') }}</p>
+            <p>Last Tour: {{ \Carbon\Carbon::parse($newestStartingDate)->format('m/d/Y') }}</p>
+            <p>Starting from: {{ $formattedLowestPrice }}€</p>
+            <p>Max price: {{ $formattedMaxPrice }}€</p>
         </div>
-        <div>
+        <div class="flex gap-5">
             <a href="{{ route('travels.show', ['slug' => $travel->slug]) }}" id="view-{{ $travel->id }}">View</a>
             @can('isEditor')
                 <a href="{{ route('travels.edit', ['slug' => $travel->slug]) }}" id="edit-{{ $travel->id }}">Edit</a>
